b1a551194f85cf0e50d38932a5102555791450bd
alvaro garcia <alvaro.garcia@codurance.com>
2016-06-08 11:57:29 +0100

    spike: test a public website for its http status

--- test/endtoend/http.js
+++ test/endtoend/http.js
@@ -1,9 +1,28 @@
-#!/usr/bin/env node
+var request = require('superagent'); 
+var superagent = require('superagent'); 
+//var assert = require('chai').assert;
+var should = require('should'); 
+var assert = require('assert');
 
-var assert = require('chai').assert;
 describe('canary', function() {
     it('truthy test', function () {
       assert.equal(true, true);
   });
 });
 
+
+describe('REST API', function(){
+    var url = 'http://lavanguardia.es:80';
+    var server = request('http://lavanguardia.es:80');
+    it('gets the names', function(done){
+		superagent
+		.get(url)
+		.end(function(err,res){
+            should.not.exist(err);
+
+		  res.status.should.equal(200);
+          console.log(res.body);
+		  done();
+		});
+    });
+});
