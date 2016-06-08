048064e5ba2e68b495ecd51ccfd2355cb7053815
alvaro garcia <alvaro.garcia@codurance.com>
2016-06-08 16:31:39 +0100

    refactor: move duplication towards the tests, for userRepository

--- test/endtoend/http.js
+++ test/endtoend/http.js
@@ -49,10 +49,32 @@ describe('exercising the test framework', function(){
     });
 });
 
+var inMemoryDB = {
+    values: [ 
+        {'id':'8', 'name':'john'},
+        {'id':'7', 'name':'jane'},
+    ],
+    findAll: function(){
+        return this.values;
+    },
+    findById: function(id){
+        var matching = this.values.filter(function(current){
+            return current.id === id;
+        });
+        if(matching.length > 0){
+            return matching[0];
+        } else {
+            return null;
+        }
+    },
+
+};
+
+
 describe('REST API', function(){
     before(function(){
         var startTheServer = function(){
-            require("../../src/server").start();
+            require("../../src/server").start(inMemoryDB);
         };
         startTheServer();
     })
