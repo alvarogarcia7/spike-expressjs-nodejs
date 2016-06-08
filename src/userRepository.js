204a6a26e713c57242edf36e9500e69a697a2a62
alvaro garcia <alvaro.garcia@codurance.com>
2016-06-08 18:09:24 +0100

    refactor: whitespace

--- src/userRepository.js
+++ src/userRepository.js
@@ -7,28 +7,25 @@ var newId = function(){
     return '9';
 };
 
-
-
 var inMemoryDB = {
-        findAll: function(){
-            return values;
-        },
-        findById: function(id){
-            var matching = values.filter(function(current){
-                return current.id === id;
-            });
-            return matching[0];
-        },
-        add: function(user){
-            user = this.tag(user);
-            values.push(user);
-            return user;
-        },
+    findAll: function(){
+        return values;
+    },
+    findById: function(id){
+        var matching = values.filter(function(current){
+            return current.id === id;
+        });
+        return matching[0];
+    },
+    add: function(user){
+        user = this.tag(user);
+        values.push(user);
+        return user;
+    },
     tag: function(user){
         user.id = newId();
         return user;
     },
-
-    };
+};
 
 module.exports = inMemoryDB;
