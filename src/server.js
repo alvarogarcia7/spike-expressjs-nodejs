c1e85e0c99d131e418a68b76987263474efdcc09
alvaro garcia <alvaro.garcia@codurance.com>
2016-06-08 14:29:14 +0100

    refactor: delete dead code

--- src/server.js
+++ src/server.js
@@ -1,17 +1,10 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 
 var app = express();
-//app.use(bodyParser);
 
 var db = [ {'id':'8', 'name':'john'} ]; 
 
 app.get('/user/:userId', function(req, res) {
-  //req.collection.find({},{limit:10, sort: [['_id',-1]]}).toArray(function(e, results){
-  //  if (e) return next(e)
-  //  res.send(results)
-  //})
-
 	db.forEach(function(current, index){
         if(current.id === req.params.userId){
             res.send(current);
