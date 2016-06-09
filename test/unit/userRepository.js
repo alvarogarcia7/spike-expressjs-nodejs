bebc758b49dc092cf40ac3e6e140bf03d93547ee
alvaro garcia <alvaro.garcia@codurance.com>
2016-06-09 20:41:03 +0100

    refactor: remove redundant tests

--- test/unit/userRepository.js
+++ test/unit/userRepository.js
@@ -7,24 +7,16 @@ describe('User Repository', function(){
         repository = new repository_.instance();
     });
 
-    it('retrieves users', function(done){
-        (repository.findById('8').id).should.equal('8');
-        done();
-    });
-
-    it('retrieves all users', function(done){
-        (repository.findAll().length).should.equal(2);
-        done();
-    });
-
     it('stores a user', function(done){
         var previousAmount = repository.findAll().length;
 
         var newUser = repository.add({'name':'james'});
         
         var currentAmount = repository.findAll().length;
+        var insertedUser = repository.findById('1');
         currentAmount.should.equal(previousAmount + 1);
         newUser.id.should.equal('1');
+        insertedUser.id.should.equal('1');
         done();
     });
 
